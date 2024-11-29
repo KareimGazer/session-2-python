2198765c7834bb231e7efe2953c220729f0154d9
Kareim Tarek <49312818+KareimGazer@users.noreply.github.com>
2024-11-29 00:49:25 +0000

    session ready

--- 3.bonus.py
+++
@@ -1,5 +0,0 @@
-
-
-# Create a program that would receive a two dimensional matrix 
-# as one row at a time and print their accumulative summation after each row, 
-# and the final accumulative summation
